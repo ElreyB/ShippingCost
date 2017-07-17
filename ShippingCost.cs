7a0f5e62aacb48df71526818cd637e14d1340696
ElreyB <elbelmonti@gmail.com>
2017-07-17 14:37:16 -0700

    give you a esitmated shipping cost

--- ShippingCost.cs
+++ ShippingCost.cs
@@ -2,21 +2,54 @@ using System;
 
 class ShippingCost
 {
+  // static void Main()
+  // {
+  //   PackageQuestionIs("How much does your package weight?");
+  //   PackageQuestionIs("How far will it travel?");
+  //   PackageQuestionIs("How much to mail package?");
+  // }
+  //   static void Main()
+  //   {
+  //   Console.WriteLine(question);
+  //   string stringPackageWeight = Console.ReadLine();
+  //   Console.WriteLine(stringPackageWeight);
+  static void PackageQuestionIs(string question)
+  {
+    Console.WriteLine(question + " is:");
+  }
   static void Main()
   {
-    Console.WriteLine("How much does your package weight?");
+    PackageQuestionIs("Package weight");
     string stringPackageWeight = Console.ReadLine();
-    Console.WriteLine("How far will it travel?");
+    int packageWeight = int.Parse(stringPackageWeight);
+
+    PackageQuestionIs("Distance to travel");
     string stringDistanceToTravel = Console.ReadLine();
-    Console.WriteLine("How much to mail package?");
+    int distanceToTravel = int.Parse(stringDistanceToTravel);
+
+    PackageQuestionIs("Price Modifier");
     string stringPriceModifier = Console.ReadLine();
+    int priceModifier = int.Parse(stringPriceModifier);
 
-    int intPackageWeight = int.Parse(stringPackageWeight);
-    int intDistanceToTravel = int.Parse(stringDistanceToTravel);
-    int intPriceModifier = int.Parse(stringPriceModifier);
-    int intEstimatedShippingCost = ( intPackageWeight / 10 ) + ( intDistanceToTravel / 5 ) * intPriceModifier;
+    int intEstimatedShippingCost = ( packageWeight / 10 ) + ( distanceToTravel / 5 ) * priceModifier;
 
-    string stringEstimateShippingCost = intEstimatedShippingCost.ToString();
-    Console.WriteLine("Your estimated cost for your package is " + stringEstimateShippingCost + ".");
+    string estimatedShippingCost = intEstimatedShippingCost.ToString();
+
+    Console.WriteLine("Your estimated shipping cost is $" + estimatedShippingCost + ".00.");
+  // First iteration
+    // Console.WriteLine("How much does your package weight?");
+    // string stringPackageWeight = Console.ReadLine();
+    // Console.WriteLine("How far will it travel?");
+    // string stringDistanceToTravel = Console.ReadLine();
+    // Console.WriteLine("How much to mail package?");
+    // string stringPriceModifier = Console.ReadLine();
+    //
+    // int intPackageWeight = int.Parse(stringPackageWeight);
+    // int intDistanceToTravel = int.Parse(stringDistanceToTravel);
+    // int intPriceModifier = int.Parse(stringPriceModifier);
+    // int intEstimatedShippingCost = ( intPackageWeight / 10 ) + ( intDistanceToTravel / 5 ) * intPriceModifier;
+    //
+    // string stringEstimateShippingCost = intEstimatedShippingCost.ToString();
+    // Console.WriteLine("Your estimated cost for your package is " + stringEstimateShippingCost + ".");
   }
 }
