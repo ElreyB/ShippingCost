30e3271c09b24c186a531999857259b7f9e0a940
ElreyB <elbelmonti@gmail.com>
2017-07-16 19:56:35 -0700

    give the estimate shipping cost

--- ShippingCost.cs
+++ ShippingCost.cs
@@ -1,4 +1,22 @@
 using System;
+
+class ShippingCost
 {
-  
+  static void Main()
+  {
+    Console.WriteLine("How much does your package weight?");
+    string stringPackageWeight = Console.ReadLine();
+    Console.WriteLine("How far will it travel?");
+    string stringDistanceToTravel = Console.ReadLine();
+    Console.WriteLine("How much to mail package?");
+    string stringPriceModifier = Console.ReadLine();
+
+    int intPackageWeight = int.Parse(stringPackageWeight);
+    int intDistanceToTravel = int.Parse(stringDistanceToTravel);
+    int intPriceModifier = int.Parse(stringPriceModifier);
+    int intEstimatedShippingCost = ( intPackageWeight / 10 ) + ( intDistanceToTravel / 5 ) * intPriceModifier;
+
+    string stringEstimateShippingCost = intEstimatedShippingCost.ToString();
+    Console.WriteLine("Your estimated cost for your package is " + stringEstimateShippingCost + ".");
+  }
 }
